c16dccdfbf49cff9bee466ee1409b48ed82b7bfa
Omkar B <omkarbacha25@gmail.com>
2025-06-05 22:01:48 +0530

    fix: rename name and lastname fields to firstName and lastName in user schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  lastname: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["ADMIN", "SALESPERSON"], default: "SALESPERSON" },
