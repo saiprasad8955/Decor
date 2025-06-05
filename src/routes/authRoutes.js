1837de527c85757ae444825cbf58280993c9cbc2
Omkar B <omkarbacha25@gmail.com>
2025-06-05 22:14:08 +0530

    fix: add bcrypt dependency for password hashing and update package-lock

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
