6174c85f65f6a201c991828c2956468ec6ff46e7
Omkar B <omkarbacha25@gmail.com>
2025-06-05 21:09:27 +0530

    fix: remove unused model imports from dashboardRoutes

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Invoice = require("../models/Invoice");
-const Item = require("../models/Item");
 
 // 1. Total Sales Today
 router.get("/sales/today", async (req, res) => {
